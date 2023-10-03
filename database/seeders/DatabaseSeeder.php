0a3939c93e61e2ab5b1bd9411c5c761ec266026e
lodaogos <leecharles565@gmail.com>
2023-10-03 15:33:07 +0700

    merapikan kode

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Jobs;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(4)->create();
+        User::factory(5)->create();
+        Jobs::factory(5)->create();
     }
 }
