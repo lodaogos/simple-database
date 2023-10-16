54dad57805429d901c685d210317a54c2a15d6ca
lodaogos <leecharles565@gmail.com>
2023-10-16 21:34:51 +0700

    tambah login dan register menggunakan laravel breeze blade

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DataController;
-use App\Http\Controllers\JobsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,16 +14,18 @@ use App\Http\Controllers\JobsController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [DataController::class, 'index'])->name('data');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/jobs', [JobsController::class, 'index'])->name('jobs');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/form', function() {
-    return view('form');
-})->name('form');
-
-Route::post('/create', [DataController::class, 'create']);
+require __DIR__.'/auth.php';
